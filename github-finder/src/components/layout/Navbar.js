da950a3aa7379b1cb8594cf5855c1e5f5cdcdd8c
Daniel Kunkler <kunkl007@umn.edu>
2020-06-09 18:40:21 -0500

    Added my first user to learn Component State

--- github-finder/src/components/layout/Navbar.js
+++ github-finder/src/components/layout/Navbar.js
@@ -12,6 +12,7 @@ export class Navbar extends Component {
     title: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired
   }
+  
   render() {
     return (
       <nav className= "navbar bg-primary">
