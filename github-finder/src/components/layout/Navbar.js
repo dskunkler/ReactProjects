991ada17dd0f09fb7139ea45b8caed838b43635a
Daniel Kunkler <kunkl007@umn.edu>
2020-06-11 21:48:00 -0500

    Added Link from react-router-dom and added Link to's instead of a ref's for Home and about. Now search persists

--- github-finder/src/components/layout/Navbar.js
+++ github-finder/src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = /*(props)*/ ({ icon, title }) => {
   return (
@@ -8,6 +9,14 @@ const Navbar = /*(props)*/ ({ icon, title }) => {
         <i className={/*props.*/ icon} />
         {/*props.*/ title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
